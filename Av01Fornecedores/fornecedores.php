3b1a412a4ec6552ecd161565afb7a81c603e4c4d
L1ppi <filippicgodoy@gmail.com>
2025-04-11 21:18:08 -0300

    Add files via upload

---
+++ Av01Fornecedores/fornecedores.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="shortcut icon" type="img/x-icon" href="./img/icone.png">
+    <title>Login</title>
+    <title>Lançamento de dados</title>
+</head>
+<body>
+    <!-- Criando campo da segunda parte -->
+    <div class="campo2">
+        <!-- Cabeçalho da segunda parte -->
+        <div class="cabeçalho2">
+        <p>Lançamento de Dados:</p>
+    </div>
+    <!-- Criando formulario da segunda parte -->
+        <div class="form2">
+            <div class="input2">
+                <input class="in" type="text" name="cnpjForn" placeholder="CNPJ" required >
+            </div>
+
+            <div class="input2">
+                <input class="in" type="text" name="razaoSocialForn" placeholder="Razão Social" required >
+            </div>
+
+            <div class="input2">
+                <input class="in" type="text" name="nomeFantasiaForn" placeholder="Nome Fantasia" required >
+            </div>
+
+            <div class="input2">
+                <input class="in" type="text" name="nomePessoaForn" placeholder="Nome">
+            </div>
+
+            <div class="input2">
+                <input class="in" type="text" name="telefonePessoaForn" placeholder="Telefone" required >
+            </div>
+
+            <div class="input2">
+                <input class="in" type="text" name="emailPessoaForn" placeholder="E-mail ">
+            </div>
+            <div class="divisor"><p>Endereço:</p></div>
+            <div class="input2">
+                <input class="in" type="text" name="cepForn" placeholder="Cep" required >
+            </div>
+
+            <div class="input2">
+                <input class="in" type="text" name="ruaForn" placeholder="Rua" required >
+            </div>
+
+            <div class="input2">
+                <input class="in" type="text" name="numForn" placeholder="Numero" required >
+            </div>
+            <div class="input2">
+                <input class="in" type="text" name="complementoForn" placeholder="Complemento" required >
+            </div>
+            <div class="input2">
+                <input class="in" type="text" name="bairroForn" placeholder="Bairro" required >
+            </div>
+            <div class="input2">
+                <input class="in" type="text" name="cidadeForn" placeholder="Cidade" required >
+            </div>
+            <div class="input2">
+                <input class="in" type="text" name="estadoForn" placeholder="Estado" required >
+            </div>
+            
+            <!--Botão de enviar ainda não funcional-->
+
+                <button class="b2">Enviar</button>
+
+        </div>
+    </div>
+</body>
+</html>
---
+++ Av01Fornecedores/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <!-- Colocando imagem de icone -->
+    <link rel="shortcut icon" type="img/x-icon" href="./img/icone.png">
+    <title>Login</title>
+</head>
+
+<body>
+    <!-- Criando campo da primeira parte -->
+    <div class="campo1">
+        <!-- Cabeçalho da primeira parte -->
+        <div class="cabeçalho1">
+        <p>Login</p>
+        <img src="./img/login.png" alt="">
+    </div>
+    <!-- Criando formulario da primeira parte -->
+        <div class="form1">
+            <div class="input1">
+                <input type="text" name="usuario" placeholder="Digite o usuario" required class="in1">
+            </div>
+            <div class="input1">
+                <input type="password" name="senha" placeholder="Digite sua senha" required class="in1">
+            </div>
+            <!-- Fazendo o botão que manda para a proxima parte -->
+            <a href="./fornecedores.html">
+                <button class="b1">Entrar</button>
+            </a>
+        </div>
+    </div>
+</body>
+
+</html>
